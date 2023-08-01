0148937dc09633ac548b01fadee64674ce966ce0
YunusEmreCinbolat <yunus.emre.cinbolat@gmail.com>
2023-08-01 20:18:40 +0300

    .

--- uygulama-seansi3/1.alistirma/app/src/main/java/com/example/a1alistirma/main.kt
+++ uygulama-seansi3/1.alistirma/app/src/main/java/com/example/a1alistirma/main.kt
@@ -1,2 +1,15 @@
 package com.example.a1alistirma
 
+fun main() {
+    val myList = listOf(1, 2, 3, 4, 5)
+    val sum = sumListElements(myList)
+    println("Toplam: $sum")
+}
+
+fun sumListElements(list: List<Int>): Int {
+    var sum = 0
+    for (element in list) {
+        sum += element
+    }
+    return sum
+}
