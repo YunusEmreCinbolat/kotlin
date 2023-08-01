87700bf37cfd7a9831358d2a9e4a83cb94c41172
YunusEmreCinbolat <yunus.emre.cinbolat@gmail.com>
2023-08-01 20:30:54 +0300

    .

--- uygulama-seansi3/2.alistirma/app/src/main/java/com/example/myapp/Main.kt
+++ uygulama-seansi3/2.alistirma/app/src/main/java/com/example/myapp/Main.kt
@@ -1,2 +1,11 @@
 package com.example.myapp
 
+fun main() {
+    val myList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val filteredList = filterOddNumbers(myList)
+    println("Tek Sayılar: $filteredList")
+}
+
+fun filterOddNumbers(list: List<Int>): List<Int> {
+    return list.filter { it % 2 != 0 }
+}
