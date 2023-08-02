ae15c2bf2beaaed933991df1851bab9f124387cf
YunusEmreCinbolat <yunus.emre.cinbolat@gmail.com>
2023-08-02 20:30:42 +0300

    .

--- uygulama-seansi4/app/src/main/java/com/example/myapp/1.odev.kt
+++ uygulama-seansi4/app/src/main/java/com/example/myapp/1.odev.kt
@@ -1,2 +1,24 @@
 package com.example.myapp
 
+import kotlin.concurrent.thread
+
+fun main() {
+    val list = listOf(1, 5, 8, 2, 10, 3, 6, 4, 7, 9)
+
+    val findMaxThread = thread {
+        val max = list.maxOrNull()
+        println("Max value: $max")
+        Thread.sleep(100)
+    }
+
+    val findMinThread = thread {
+        val min = list.minOrNull()
+        println("Min value: $min")
+        Thread.sleep(100)
+    }
+
+    findMaxThread.join()
+    findMinThread.join()
+
+    println("Both threads have finished.")
+}
