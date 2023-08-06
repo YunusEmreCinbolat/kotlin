3d2b546f2cd3c46c064f06bb808af28321a52862
YunusEmreCinbolat <yunus.emre.cinbolat@gmail.com>
2023-08-06 15:44:16 +0300

    .

---
+++ konu-anlatimi3/app/src/main/java/com/example/login/RegisterMainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_main)
+    }
+}
