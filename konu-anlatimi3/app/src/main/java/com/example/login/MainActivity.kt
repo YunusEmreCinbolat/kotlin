d7e2a673ad2d1050942c71cfb57f665198e12709
YunusEmreCinbolat <yunus.emre.cinbolat@gmail.com>
2023-08-06 15:44:30 +0300

    .

--- konu-anlatimi3/app/src/main/java/com/example/login/MainActivity.kt
+++ konu-anlatimi3/app/src/main/java/com/example/login/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.login
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -10,52 +11,34 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import com.example.login.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+        val ad = binding.editTextGirisAd
+        val sifre = binding.editTextTextGirisSifre
+
         setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAnchorView(R.id.fab)
-                .setAction("Action", null).show()
+        binding.buttonGiris.setOnClickListener {
+            if (!ad.text.isEmpty() || !sifre.text.isEmpty()) {
+                val giris = Intent(this, LoginMainActivity::class.java)
+                startActivity(giris)
+            } else {
+                Toast.makeText(this, "Bos alanlar", Toast.LENGTH_LONG).show()
+            }
         }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        binding.buttonkayit.setOnClickListener {
+            val kayitol = Intent(this, RegisterMainActivity::class.java)
+            startActivity(kayitol)
         }
-    }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
+
--- konu-anlatimi3/app/src/main/java/com/example/login/RegisterMainActivity.kt
+++ konu-anlatimi3/app/src/main/java/com/example/login/RegisterMainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import com.example.login.databinding.ActivityRegisterMainBinding
 
 class RegisterMainActivity : AppCompatActivity() {
+    public lateinit var binding: ActivityRegisterMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_main)
+        binding= ActivityRegisterMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        val ad=binding.editName
+        val soyad=binding.editSoyad
+        val sifre=binding.editTextTextSifre
+        val sifreTekrar=binding.editTextTextSifreTekrar
+        val email=binding.editTextTextEmailAddress
+        binding.btnkayit.setOnClickListener { if(!ad.text.isEmpty()|| !soyad.text.isEmpty() || ! sifre.text.isEmpty() || sifreTekrar.text.isEmpty() || !email.text.isEmpty()){
+            val girisekran=Intent(this,MainActivity::class.java)
+        } else
+            Toast.makeText(this,"Yanlis girdi",Toast.LENGTH_LONG).show()
+        }
     }
 }
