9e6032cefc18204f009fe7ccb53b36881b07896c
YunusEmreCinbolat <yunus.emre.cinbolat@gmail.com>
2023-08-07 15:32:52 +0300

    .

--- 4.hafta/1.odev/app/src/main/java/com/example/myapp/MainActivity.kt
+++ 4.hafta/1.odev/app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.myapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.myapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val bosmuAd=binding.editTextAd
+        val bosmuTelefon=binding.editTextTelefon
+        binding.buttonkaydet.setOnClickListener {
+            if(!bosmuAd.text.isEmpty() || !bosmuTelefon.text.isEmpty()){
+                val kaydet=Intent(this,KayitActivity::class.java)
+                startActivity(kaydet)
+            }
+            else{
+                Toast.makeText(this,"Bos alanlari doldurun",Toast.LENGTH_LONG).show()
+            }
+        }
+
     }
 }
