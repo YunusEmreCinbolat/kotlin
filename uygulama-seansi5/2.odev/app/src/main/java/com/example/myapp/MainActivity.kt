82261e8623e541342fed50157b0341364830b813
YunusEmreCinbolat <yunus.emre.cinbolat@gmail.com>
2023-08-08 21:54:44 +0300

    .

--- uygulama-seansi5/2.odev/app/src/main/java/com/example/myapp/MainActivity.kt
+++ uygulama-seansi5/2.odev/app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.myapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    public lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        binding.buttondetay1.setOnClickListener {
+            val sayfa= Intent(this,detayBirActivity::class.java)
+            startActivity(sayfa)
+        }
+        binding.buttondetay2.setOnClickListener {
+            val sayfa= Intent(this,detayIkiActivity::class.java)
+            startActivity(sayfa)
+        }
+        binding.buttondetay3.setOnClickListener {
+            val sayfa= Intent(this,DetayUcActivity::class.java)
+            startActivity(sayfa)
+        }
+        binding.buttondetay4.setOnClickListener {
+            val sayfa= Intent(this,DetayDortActivity::class.java)
+            startActivity(sayfa)
+        }
     }
 }
