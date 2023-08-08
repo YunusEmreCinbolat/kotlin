b360917ec0a47d2df2587cb951903bd8a1797f0f
YunusEmreCinbolat <yunus.emre.cinbolat@gmail.com>
2023-08-08 20:59:12 +0300

    .

---
+++ uygulama-seansi5/1.odev/app/src/main/java/com/example/myapp/AnasayfaMainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AnasayfaMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_anasayfa_main)
+    }
+}
