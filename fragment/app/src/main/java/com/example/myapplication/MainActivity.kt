b01bf07dccbe762fbcdba42c03cb739342af1396
YunusEmreCinbolat <yunus.emre.cinbolat@gmail.com>
2023-08-18 14:30:52 +0300

    .

--- fragment/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ fragment/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,18 +8,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        fun btniki(view:View){
-            val fragmentManager=supportFragmentManager
-            val fragmentTransaction=fragmentManager.beginTransaction()
-            val secondFragment=BlankFragment2()
-            fragmentTransaction.replace(R.id.frameLayout,secondFragment).commit()
-        }
-        fun btnbir(view: View){
-            val fragmentManager=supportFragmentManager
-            val fragmentTransaction=fragmentManager.beginTransaction()
-            val firstFragment=BlankFragment()
-            fragmentTransaction.replace(R.id.frameLayout,firstFragment).commit()
 
-        }
+    }
+    fun btniki(view:View){
+        val fragmentManager=supportFragmentManager
+        val fragmentTransaction=fragmentManager.beginTransaction()
+        val secondFragment=BlankFragment2()
+        fragmentTransaction.replace(R.id.frameLayout,secondFragment).commit()
+    }
+    fun btnbir(view: View){
+        val fragmentManager=supportFragmentManager
+        val fragmentTransaction=fragmentManager.beginTransaction()
+        val firstFragment=BlankFragment()
+        fragmentTransaction.replace(R.id.frameLayout,firstFragment).commit()
+
     }
 }
