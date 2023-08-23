74f3b57e74dbb47fa30c1acc7a0661af807646bb
YunusEmreCinbolat@gmail.com <yunus.emre.cinbolat@gmail.com>
2023-08-23 08:53:22 +0300

    hata giderildi

--- 6.hafta/2.odev/app/src/main/java/com/example/myapplication/GelismeFragment.kt
+++ 6.hafta/2.odev/app/src/main/java/com/example/myapplication/GelismeFragment.kt
@@ -5,16 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.myapplication.databinding.FragmentGelismeBinding
 
 
 class GelismeFragment : Fragment() {
-
+    private lateinit var binding: FragmentGelismeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gelisme, container, false)
+        binding= FragmentGelismeBinding.inflate(inflater,container,false)
+        binding.buttonSonuc.setOnClickListener {
+            val tasima=GelismeFragmentDirections.actionGelismeFragmentToSonucFragment()
+            Navigation.findNavController(it).navigate(tasima)
+        }
+        return binding.root
     }
 
 }
--- 6.hafta/2.odev/app/src/main/java/com/example/myapplication/GirisFragment.kt
+++ 6.hafta/2.odev/app/src/main/java/com/example/myapplication/GirisFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.myapplication.databinding.FragmentGirisBinding
 
 
@@ -16,7 +17,8 @@ class GirisFragment : Fragment() {
     ): View? {
         binding= FragmentGirisBinding.inflate(inflater,container,false)
         binding.buttonGelisme.setOnClickListener {
-            val tasima = 
+            val tasima = GirisFragmentDirections.actionGirisFragmentToGelismeFragment()
+            Navigation.findNavController(it).navigate(tasima)
         }
     return binding.root
     }
