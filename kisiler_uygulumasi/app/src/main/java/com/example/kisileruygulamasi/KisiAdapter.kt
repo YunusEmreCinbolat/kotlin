c6a36fb237cff18e3c93bdac83f9733c53a1a352
YunusEmreCinbolat@gmail.com <yunus.emre.cinbolat@gmail.com>
2023-09-02 18:36:32 +0300

    edit kisiAdapter

--- kisiler_uygulumasi/app/src/main/java/com/example/kisileruygulamasi/KisiAdapter.kt
+++ kisiler_uygulumasi/app/src/main/java/com/example/kisileruygulamasi/KisiAdapter.kt
@@ -1,15 +1,19 @@
 package com.example.kisileruygulamasi
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.PopupMenu
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class KisiAdapter(private val mContext: Context,private val kisiList: List<kisiler>): RecyclerView.Adapter<KisiAdapter.KisiTutucu>() {
     inner class KisiTutucu(val tasarim: View ):RecyclerView.ViewHolder(tasarim){
         val KisiAd_tel:TextView=tasarim.findViewById(R.id.textViewAdTelefon)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): KisiTutucu {
@@ -21,9 +25,11 @@ class KisiAdapter(private val mContext: Context,private val kisiList: List<kisil
        return kisiList.size
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: KisiTutucu, position: Int) {
        val kisi=kisiList[position]
         holder.KisiAd_tel.text="${kisi.kisi_ad} - ${kisi.kisi_tel}"
+
 
     }
 }
--- kisiler_uygulumasi/app/src/main/java/com/example/kisileruygulamasi/MainActivity.kt
+++ kisiler_uygulumasi/app/src/main/java/com/example/kisileruygulamasi/MainActivity.kt
@@ -2,14 +2,35 @@ package com.example.kisileruygulamasi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kisileruygulamasi.databinding.ActivityMainBinding
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
     private lateinit var  binding:ActivityMainBinding
-    
+    private lateinit var kisiler: ArrayList<kisiler>
+    private lateinit var kisiAdapter: KisiAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.toolbar.title="Kisi uygulaması"
+        setSupportActionBar(binding.toolbar)
+        binding.rv.setHasFixedSize(true)
+
+        binding.rv.layoutManager=LinearLayoutManager(this)
+        kisiler=ArrayList()
+        val kisi1=kisiler("1","yunus","053214")
+        val kisi2=kisiler("1","yunus","053214")
+        val kisi3=kisiler("1","yunus","053214")
+        val kisi4=kisiler("1","yunus","053214")
+        kisiler.add(kisi1)
+        kisiler.add(kisi2)
+        kisiler.add(kisi3)
+        kisiler.add(kisi4)
+        kisiAdapter= KisiAdapter(applicationContext,kisiler)
+        binding.rv.adapter=kisiAdapter
     }
 }
