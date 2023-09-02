4d8313225ba41eee2b6ff5d7b9d0fa095b2a616f
YunusEmreCinbolat@gmail.com <yunus.emre.cinbolat@gmail.com>
2023-09-02 21:45:39 +0300

    activated delete and update buttons activated

--- kisiler_uygulumasi/app/src/main/java/com/example/kisileruygulamasi/KisiAdapter.kt
+++ kisiler_uygulumasi/app/src/main/java/com/example/kisileruygulamasi/KisiAdapter.kt
@@ -1,35 +1,81 @@
 package com.example.kisileruygulamasi
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.PopupMenu
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.example.kisileruygulamasi.databinding.ListItemBinding
+import com.google.android.material.snackbar.Snackbar
 
 class KisiAdapter(private val mContext: Context,private val kisiList: List<kisiler>): RecyclerView.Adapter<KisiAdapter.KisiTutucu>() {
-    inner class KisiTutucu(val tasarim: View ):RecyclerView.ViewHolder(tasarim){
-        val KisiAd_tel:TextView=tasarim.findViewById(R.id.textViewAdTelefon)
+    inner class KisiTutucu(val binding: ListItemBinding ):RecyclerView.ViewHolder(binding.root){
+
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): KisiTutucu {
-        val tasarim=LayoutInflater.from(mContext).inflate(R.layout.list_item,parent,false)
-        return KisiTutucu(tasarim)
+       val binding =ListItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return KisiTutucu(binding)
     }
 
     override fun getItemCount(): Int {
        return kisiList.size
     }
 
-    @SuppressLint("SetTextI18n")
+
     override fun onBindViewHolder(holder: KisiTutucu, position: Int) {
        val kisi=kisiList[position]
-        holder.KisiAd_tel.text="${kisi.kisi_ad} - ${kisi.kisi_tel}"
+        val binding=holder.binding
+        binding.textViewAdTelefon.text="${kisi.kisi_ad} - ${kisi.kisi_tel}"
+        binding.imageViewMore.setOnClickListener {
+            val popupMenu=PopupMenu(mContext,binding.imageViewMore)
+            popupMenu.menuInflater.inflate(R.menu.popup_menu,popupMenu.menu)
+            popupMenu.setOnMenuItemClickListener {
+                menuItem ->
+                when(menuItem.itemId){
+                    R.id.KisiGuncelle ->{
+                        showAlert(kisi)
+                        true
+                    }
+                    R.id.KisiSil -> {
+                      Snackbar.make(it,"Silinsin mi",Snackbar.LENGTH_LONG).setAction("Evet"){}.show()
+                        true
+                    }
+                    else -> false
+                }
+
+
+            }
+            popupMenu.show()
+        }
 
 
     }
+    private fun showAlert(user: kisiler){
+        val design=LayoutInflater.from(mContext).inflate(R.layout.alert_design,null)
+        val editTextName=design.findViewById(R.id.name_et) as EditText
+        val  editTextphone=design.findViewById(R.id.phone_et) as EditText
+
+        editTextName.setText(user.kisi_ad)
+        editTextphone.setText(user.kisi_tel)
+
+        val alertDialog=AlertDialog.Builder(mContext)
+            .setTitle("Kisi Güncelle")
+            .setView(design)
+            .setPositiveButton("Güncelle"){dialogInterface,i ->
+                val name = editTextName.text.toString().trim()
+                val phone=editTextphone.text.toString().trim()
+                Toast.makeText(mContext,"$name - $phone ",Toast.LENGTH_LONG).show()
+            }
+            .setNegativeButton("İptal"){ dialogInterface, i ->}.create().show()
+    }
+
 }
--- kisiler_uygulumasi/app/src/main/java/com/example/kisileruygulamasi/MainActivity.kt
+++ kisiler_uygulumasi/app/src/main/java/com/example/kisileruygulamasi/MainActivity.kt
@@ -2,35 +2,47 @@ package com.example.kisileruygulamasi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Adapter
-import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kisileruygulamasi.databinding.ActivityMainBinding
-import java.util.ArrayList
+import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var  binding:ActivityMainBinding
-    private lateinit var kisiler: ArrayList<kisiler>
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var kisilerArrayList: ArrayList<kisiler>
     private lateinit var kisiAdapter: KisiAdapter
+    private val database = FirebaseDatabase.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.toolbar.title="Kisi uygulaması"
+        binding.toolbar.title = "Kisi uygulaması"
         setSupportActionBar(binding.toolbar)
         binding.rv.setHasFixedSize(true)
 
-        binding.rv.layoutManager=LinearLayoutManager(this)
-        kisiler=ArrayList()
-        val kisi1=kisiler("1","yunus","053214")
-        val kisi2=kisiler("1","yunus","053214")
-        val kisi3=kisiler("1","yunus","053214")
-        val kisi4=kisiler("1","yunus","053214")
-        kisiler.add(kisi1)
-        kisiler.add(kisi2)
-        kisiler.add(kisi3)
-        kisiler.add(kisi4)
-        kisiAdapter= KisiAdapter(applicationContext,kisiler)
-        binding.rv.adapter=kisiAdapter
+        binding.rv.layoutManager = LinearLayoutManager(this)
+        getDataFromFirebase()
+    }
+
+    private fun getDataFromFirebase() {
+        val refUser = database.getReference("kisiler")
+        refUser.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                kisilerArrayList = ArrayList()
+                for (data in snapshot.children) {
+                    val user = data.getValue(kisiler::class.java) // Kisi sınıfının doğru kullanımı
+                    if (user != null) {
+                        kisilerArrayList.add(user)
+                    }
+                }
+                kisiAdapter = KisiAdapter(applicationContext, kisilerArrayList)
+                binding.rv.adapter = kisiAdapter
+                kisiAdapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Hata işleme stratejisi belirtmek gerekiyor
+            }
+        })
     }
 }
