67c7fa16622b9c9118abfe3d57dd2586ea0bea20
YunusEmreCinbolat@gmail.com <yunus.emre.cinbolat@gmail.com>
2023-09-04 16:30:08 +0300

    add read data firebase function

--- FirebasAuthenticationLesson2/app/src/main/java/com/example/firebasauthenticationlesson2/MainActivity.kt
+++ FirebasAuthenticationLesson2/app/src/main/java/com/example/firebasauthenticationlesson2/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.firebasauthenticationlesson2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.firebasauthenticationlesson2.databinding.ActivityMainBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -18,10 +22,30 @@ class MainActivity : AppCompatActivity() {
 
         //refNesne.child("-NdVHuTCle0MMgpevARc").removeValue()
 
-        val updateInfo= HashMap<String,Any>()
+       /* val updateInfo= HashMap<String,Any>()
         updateInfo["nenneAdi"]="Pasta"
         updateInfo["nesneAdet"]=12
         updateInfo["nesneAlani"]="Tatlı"
         refNesne.child("-NdVITgAzp35TgG_R0HL").updateChildren(updateInfo)
+    */
+        refNesne.addValueEventListener(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for ( deger in snapshot.children) {
+                    val nesne = deger.getValue(Nesneler::class.java)
+                    if(nesne!=null){
+                        Log.e("Nesne ad",nesne.nenneAdi.toString())
+                        Log.e("Nesne adet",nesne.nesneAdet.toString())
+                        Log.e("Kullanım alanı",nesne.nesneAlani.toString())
+
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
+
 }
