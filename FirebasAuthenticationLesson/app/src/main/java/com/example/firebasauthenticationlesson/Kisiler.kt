be44a17555941366337de0ea83aee0ffbba8606f
YunusEmreCinbolat@gmail.com <yunus.emre.cinbolat@gmail.com>
2023-09-04 13:49:00 +0300

    add database kisiler class

---
+++ FirebasAuthenticationLesson/app/src/main/java/com/example/firebasauthenticationlesson/Kisiler.kt
@@ -0,0 +1,6 @@
+package com.example.firebasauthenticationlesson
+
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+data class Kisiler(var kisiAd:String?="",var kisiYas:Int?=0)
--- FirebasAuthenticationLesson/app/src/main/java/com/example/firebasauthenticationlesson/MainActivity.kt
+++ FirebasAuthenticationLesson/app/src/main/java/com/example/firebasauthenticationlesson/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.firebasauthenticationlesson
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.firebasauthenticationlesson.databinding.ActivityMainBinding
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val database=FirebaseDatabase.getInstance()
+
+        val refKisiler=database.getReference("kisiler")
+        
+        val kisi= Kisiler("ali",5)
+        refKisiler.push().setValue(kisi)
     }
 }
