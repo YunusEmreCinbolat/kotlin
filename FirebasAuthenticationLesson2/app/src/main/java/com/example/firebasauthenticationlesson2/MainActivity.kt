ff58855e6c8a93b75b7285cd8d8bd04c19039c8f
YunusEmreCinbolat@gmail.com <yunus.emre.cinbolat@gmail.com>
2023-09-04 16:10:09 +0300

    add firebase class

--- FirebasAuthenticationLesson2/app/src/main/java/com/example/firebasauthenticationlesson2/MainActivity.kt
+++ FirebasAuthenticationLesson2/app/src/main/java/com/example/firebasauthenticationlesson2/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.firebasauthenticationlesson2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.firebasauthenticationlesson2.databinding.ActivityMainBinding
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var  database=FirebaseDatabase.getInstance()
+        val refNesne=database.getReference("nesneler")
+       /* val nesne = Nesneler("Havuç",9,"Sebze")
+        refNesne.push().setValue(nesne)*/
     }
 }
