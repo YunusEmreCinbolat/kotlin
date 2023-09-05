db62bef8bd51d2089511a7ce6f259117f4e002c3
YunusEmreCinbolat@gmail.com <yunus.emre.cinbolat@gmail.com>
2023-09-05 21:57:29 +0300

    create designs

---
+++ notlar_uygulamasi/app/src/main/java/com/example/notesapp/notesAdd.kt
@@ -0,0 +1,11 @@
+package com.example.notesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class notesAdd : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notes_add)
+    }
+}
