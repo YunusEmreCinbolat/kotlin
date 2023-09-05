ff5f110ae3256bdbbed00daacc58f4dfc49df9d5
YunusEmreCinbolat@gmail.com <yunus.emre.cinbolat@gmail.com>
2023-09-05 17:36:39 +0300

    create and edit KategorilerAdapter.kt

---
+++ CocukEglence/app/src/main/java/com/example/cocukeglence/KategorilerAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.cocukeglence
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+
+class KategorilerAdapter(private  val mContext: Context,private val kategoriListe:List<Kategoriler>):RecyclerView.Adapter<KategorilerAdapter.CardTasarimTutucu>() {
+
+    inner class CardTasarimTutucu(tasarim: View):RecyclerView.ViewHolder(tasarim){
+        var kategori_card:CardView
+        var textViewKategoriAd:TextView
+        init {
+            kategori_card=tasarim.findViewById(R.id.kategori_card)
+            textViewKategoriAd=tasarim.findViewById(R.id.textViewKategoriAd)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardTasarimTutucu {
+        val tasarim=LayoutInflater.from(parent.context).inflate(R.layout.kategori_card_tasarim,parent,false)
+        return CardTasarimTutucu(tasarim)
+    }
+
+    override fun getItemCount(): Int {
+        return kategoriListe.size
+    }
+
+    override fun onBindViewHolder(holder: CardTasarimTutucu, position: Int) {
+        val kategoriler= kategoriListe.get(position)
+        holder.textViewKategoriAd.text=kategoriler.kategori_ad
+        holder.kategori_card.setOnClickListener {
+            
+        }
+    }
+}
