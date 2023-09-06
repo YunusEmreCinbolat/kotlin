c59941a9033d4322d16ff6b0969ac1f13d9fdd25
YunusEmreCinbolat@gmail.com <yunus.emre.cinbolat@gmail.com>
2023-09-06 12:45:17 +0300

    add and edit noteAdapter.kt

---
+++ notlar_uygulamasi/app/src/main/java/com/example/notesapp/noteAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.notesapp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.notesapp.databinding.NotesCardDesignBinding
+
+class noteAdapter(private val mContext:Context,private val noteList: List<noteContent>):RecyclerView.Adapter<noteAdapter.noteHolder>() {
+    inner class noteHolder(tasarim: NotesCardDesignBinding):RecyclerView.ViewHolder(tasarim.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): noteHolder {
+        val tasarim=NotesCardDesignBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return noteHolder(tasarim)
+    }
+
+    override fun getItemCount(): Int {
+       return noteList.size
+    }
+
+    override fun onBindViewHolder(holder: noteHolder, position: Int) {
+        val note=noteList.get(position)
+        holder.itemView.findViewById<TextView>(R.id.textViewnoteTitle).text=note.note_title
+        holder.itemView.findViewById<TextView>(R.id.textViewnoteTitle).text=note.note_content
+    }
+}
