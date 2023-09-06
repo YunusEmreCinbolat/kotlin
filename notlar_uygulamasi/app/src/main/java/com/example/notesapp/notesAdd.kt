64aff1ad8083b09d7d5afd6476122bd3dfda8e48
YunusEmreCinbolat@gmail.com <yunus.emre.cinbolat@gmail.com>
2023-09-06 12:12:11 +0300

    edit activity_notes_add.xml

--- notlar_uygulamasi/app/src/main/java/com/example/notesapp/notesAdd.kt
+++ notlar_uygulamasi/app/src/main/java/com/example/notesapp/notesAdd.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class notesAdd : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notes_add)
