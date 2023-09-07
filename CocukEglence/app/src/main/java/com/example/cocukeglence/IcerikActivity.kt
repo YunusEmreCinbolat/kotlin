6409aad62738a9b555c4bf8b56f6d98dcdc7b69a
YunusEmreCinbolat@gmail.com <yunus.emre.cinbolat@gmail.com>
2023-09-07 14:12:05 +0300

    create and edit Icerikleradapter.kt

--- CocukEglence/app/src/main/java/com/example/cocukeglence/IcerikActivity.kt
+++ CocukEglence/app/src/main/java/com/example/cocukeglence/IcerikActivity.kt
@@ -2,10 +2,37 @@ package com.example.cocukeglence
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.cocukeglence.databinding.ActivityIcerikBinding
 
 class IcerikActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIcerikBinding
+    private lateinit var icerikListe: ArrayList<Icerikler>
+    private lateinit var iceriklerAdapter: IceriklerAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_icerik)
+        binding = ActivityIcerikBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.toolbarIcerikler.title = "İçerik"
+        setSupportActionBar(binding.toolbarIcerikler)
+        binding.icerikRv.setHasFixedSize(true)
+        binding.icerikRv.layoutManager =
+            StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        icerikListe = java.util.ArrayList()
+        val k = Kategoriler(1, "MASAL")
+        val y = IcerikDetay(1, "Keloğlan",)
+        val c1 = Icerikler(1, "prens", "prens", "prens", k, y)
+        val c2 = Icerikler(2, "masalevi", "masalevi", "masalevi", k, y)
+        val c3 = Icerikler(3, "pinokyo", "pinokyo", "pinokyo", k, y)
+        val c4 = Icerikler(4, "rapunzel", "rapunzel", "rapunzel", k, y)
+        val c5 = Icerikler(5, "uzgunordek", "uzgunordek", "uzgunordek", k, y)
+        icerikListe.add(c1)
+        icerikListe.add(c2)
+        icerikListe.add(c3)
+        icerikListe.add(c4)
+        icerikListe.add(c5)
+        iceriklerAdapter=IceriklerAdapter(this,icerikListe)
+        binding.icerikRv.adapter=iceriklerAdapter
     }
 }
