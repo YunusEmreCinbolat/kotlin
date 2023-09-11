dd68f3aa8ee56a20acfe63dd1e62363afc48d348
YunusEmreCinbolat@gmail.com <yunus.emre.cinbolat@gmail.com>
2023-09-11 13:53:32 +0300

    initial commit

--- CocukEglence/app/src/main/java/com/example/cocukeglence/Icerikler.kt
+++ CocukEglence/app/src/main/java/com/example/cocukeglence/Icerikler.kt
@@ -2,4 +2,4 @@ package com.example.cocukeglence
 
 import java.io.Serializable
 
-data class Icerikler(var icerik_id:Int?=0, var icerik_ad:String?="", var icerik_isim:String?="", var icerik_resim:String?="", var kategoriler: kategoriler?=null, var IcerikDetay:IcerikDetay?=null):Serializable
+data class Icerikler(var icerik_id:Long?=0, var icerik_ad:String?="", var icerik_isim:String?="", var icerik_resim:String?="", var kategoriler: kategoriler?=null, var IcerikDetay:IcerikDetay?=null):Serializable
